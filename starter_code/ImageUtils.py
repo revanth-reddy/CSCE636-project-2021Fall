fdc591caad0767b840f0579c9ca4096fc2a6a702
Revanth Reddy <revanth@connect-172-31-19-191.vpn.tamu.edu>
2021-11-14 01:44:12 -0600

    starter code

---
+++ starter_code/ImageUtils.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+"""This script implements the functions for data augmentation
+and preprocessing.
+"""
+
+def parse_record(record, training):
+    """Parse a record to an image and perform data preprocessing.
+
+    Args:
+        record: An array of shape [3072,]. One row of the x_* matrix.
+        training: A boolean. Determine whether it is in training mode.
+
+    Returns:
+        image: An array of shape [32, 32, 3].
+    """
+    ### YOUR CODE HERE
+
+    ### END CODE HERE
+
+    image = preprocess_image(image, training) # If any.
+
+    return image
+
+
+def preprocess_image(image, training):
+    """Preprocess a single image of shape [height, width, depth].
+
+    Args:
+        image: An array of shape [32, 32, 3].
+        training: A boolean. Determine whether it is in training mode.
+
+    Returns:
+        image: An array of shape [32, 32, 3]. The processed image.
+    """
+    ### YOUR CODE HERE
+
+    ### END CODE HERE
+
+    return image
+
+
+# Other functions
+### YOUR CODE HERE
+
+### END CODE HERE
